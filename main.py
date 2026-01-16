e67b8c4fb29574a760efc1290aae800d5d2e0116
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2026-01-16 12:37:38 -0800

    feat: add searxng and prompt engineering

--- main.py
+++ main.py
@@ -8,21 +8,34 @@ from rich import print
 class JobDescription(BaseModel):
     position: str
     company_desc: str
+    degree_required: Optional[str]
     qualifications: list[str]
     responsibilities: list[str]
     schedule: str
 
 
+system_prompt = """
+- Handshake is the name of a university job search platform.
+"""
+
 with open("desc.txt", "r") as f:
     desc = f.read()
+    prompt = f"""
+    Parse the following job description according to the JSON schema {JobDescription.model_json_schema()}:
+    {desc}
+    """
 
     client = OpenAI(base_url="http://192.168.1.12:8000/v1", api_key="none")
     response = client.beta.chat.completions.parse(
         model="Qwen/Qwen3-0.6B",
         messages=[
             {
+                "role": "system",
+                "content": system_prompt,
+            },
+            {
                 "role": "user",
-                "content": f"Parse the following job description according to the JSON schema {JobDescription.model_json_schema()}:\n{desc}",
+                "content": prompt,
             }
         ],
         response_format=JobDescription,
